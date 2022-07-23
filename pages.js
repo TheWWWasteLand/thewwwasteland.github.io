fac5de3276bbe98b53ac0feb1c0513dd7458b3ab
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-07-23 15:20:22 +0200

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,4 +1,4 @@
 function nextPage() {
   document.getElementById('page-i').style.display="none";
-  document.getElementById('page-2').style.display="";
+  document.getElementById('page-2').style.display="block";
 }
