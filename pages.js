8565502ca63f8fb26ad93948ed92c520a88e04e4
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-07-23 23:11:35 +0200

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,4 +1,12 @@
 function nextPage() {
   document.getElementById('page-i').style.display="none";
   document.getElementById('page-2').style.display="block";
+};
+
+function finalVer() {
+  document.getElementById('rdg-2').classList.add('total-del');
+  document.getElementById('note-1').style.display="block";
+  document.getElementById('rdg-4').style.textDecoration = "line-through";
+  document.getElementById('rdg-5').style.textDecoration = "line-through";
+  document.getElementById('rdg-6').style.textDecoration = "line-through";
 }
