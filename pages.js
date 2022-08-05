3fb521f02df861094e2ffc7da415240e26aec2bf
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-08-05 17:36:19 +0200

    Update pages.js

--- pages.js
+++ pages.js
@@ -156,10 +156,11 @@ function finalVer() {
     document.getElementById('del-19a').classList.toggle('line-del');
     document.getElementById('del-19b').classList.toggle('line-del');
     document.getElementById('del-19c').classList.toggle('line-del');
-    document.getElementById('add-9a').classList.toggle('hidden');  
-    document.getElementById('add-9b').classList.toggle('hidden');  
-    document.getElementById('add-9c').classList.toggle('hidden');  
-    document.getElementById('add-8').classList.toggle('hidden');  
+    document.getElementById('add-10a').classList.toggle('hidden');  
+    document.getElementById('add-10b').classList.toggle('hidden');  
+    document.getElementById('add-10c').classList.toggle('hidden');  
+    document.getElementById('add-8').classList.toggle('hidden'); 
+    document.getElementById('add-9').classList.toggle('hidden');  
     document.getElementById('rdg-20').classList.toggle('highlightPound');
     document.getElementById('rdg-19').classList.toggle('highlight');
     document.getElementById('rdg-23').classList.toggle('highlightPound');
