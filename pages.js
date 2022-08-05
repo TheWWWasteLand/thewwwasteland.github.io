0a42c6c16de84340338c4504e26d1ba53bdabadd
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-08-05 16:17:48 +0200

    Update pages.js

--- pages.js
+++ pages.js
@@ -141,9 +141,15 @@ function finalVer() {
     document.getElementById('note-5').classList.toggle('hidden');
   }
   else if (three.getAttribute('style') == "display: block;") {
+    const pound1 = document.createElement("img"), pound2 = document.createElement("img");
+    pound1.setAttribute('src', 'https://thewwwasteland.github.io/images/pound1.png');
+    pound1.setAttribute('id', 'pound-1');
+    pound2.setAttribute('src', 'https://thewwwasteland.github.io/images/pound2.png');
+    pound2.setAttribute('id', 'pound-2');
     document.getElementById('add-7').classList.toggle('hidden');
     document.getElementById('del-16').classList.toggle('line-del');
-    document.getElementById('del-16').classList.toggle('line-del');
+    document.getElementById('del-17').classList.toggle('line-del');
+    document.getElementById('del-18').classList.toggle('line-del');
     document.getElementById('add-8').classList.toggle('hidden');  
     document.getElementById('rdg-20').classList.toggle('highlightPound');
     document.getElementById('rdg-19').classList.toggle('highlight');
